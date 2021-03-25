f44db59758cbf8b2ce7850cc8755a4c72ae9a596
Clara Ribas <72502250+clararibas@users.noreply.github.com>
2021-03-25 10:20:40 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './componentes/Header.js';
+import Cadastro from './componentes/Cadastro.js';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Cadastro/>
+    <h1>Hello World</h1>
+    </>
+  );
+}
+
+export default App;
